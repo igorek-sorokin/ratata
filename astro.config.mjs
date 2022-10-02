5adfef650db9f96d62f3384baccd96892bdd5132
Egor Ivanov <32535410+frontninja@users.noreply.github.com>
2022-10-02 16:17:03 +0300

    header mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()]
+  integrations: [image(), alpinejs()]
 });
