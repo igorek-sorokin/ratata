0ae3d9cc4521e9c1f985841c2247864e3e3a3649
Egor Ivanov <32535410+frontninja@users.noreply.github.com>
2022-11-14 20:40:03 +0300

    payment form register

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="@astrojs/image/client" />
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_USER_NAME: string;
+    readonly PUBLIC_PASSWORD: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
