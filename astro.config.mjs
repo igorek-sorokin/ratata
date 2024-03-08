0de7ce3c69c28aede0a02a70f70460ea052616b5
egorxvc <egorxvc@gmail.com>
2024-03-08 14:46:24 +0300

    Доработки

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), mdx(), tailwind()],
+  integrations: [alpinejs(), mdx(), tailwind(), react()]
 });
---
+++ src/components/ui/Paralax.jsx
@@ -0,0 +1,47 @@
+"use client"
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { useWindowSize } from "@studio-freight/hamo";
+import { ScrollTrigger } from "../../../node_modules/gsap/ScrollTrigger.js";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const Parallax = ({className, children, speed=1, id="paralax"}) => {
+    const trigger = useRef();
+    const target = useRef();
+    const timeline = useRef();
+    const { width: windowWidth } = useWindowSize();
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+    
+        const y = windowWidth * speed * 0.1;
+        const setY = gsap.quickSetter(target.current, "y", "px");
+    
+        timeline.current = gsap.timeline({
+          scrollTrigger: {
+            id: id,
+            trigger: trigger.current,
+            scrub: true, 
+            start: "top bottom",  
+            end: "bottom top", 
+            onUpdate: (e) => {
+              setY(e.progress * y);
+            },
+          },
+        });
+    
+        return () => {
+          timeline?.current?.kill(); 
+        };
+      }, [id, speed, windowWidth]);
+
+    return (
+        <div ref={trigger} className={className}>
+            <div ref={target}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Parallax;
