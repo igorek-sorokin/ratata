4e4538680042d674a185a1efc36b36523ace060a
Igor <sorokin1991@mail.ru>
2024-07-24 11:30:43 +0400

    error npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), mdx(), tailwind(), react()]
+  integrations: [alpinejs(), mdx(), tailwind(), react()],
+  vite: {
+    build: {
+      sourcemap: false, // Отключаем карты исходников для устранения ошибки
+    },
+  },
 });
